bee762f6f048b3242157d3735ed338506e2cdcbd
dincengincan <engincandinc@gmail.com>
2019-11-15 22:23:25 +0300

    arrays

---
+++ 2.js
@@ -0,0 +1,5 @@
+var numbers = [1, 4, 9];
+
+var triples = numbers.map(item => {
+  return item * 3;
+})
---
+++ 3.js
@@ -0,0 +1,6 @@
+var numbers = [12, 5, 20, 130, 44, 9, 240]
+
+var lessThan15 = numbers.filter(item => {
+  return item < 15;
+})
+
